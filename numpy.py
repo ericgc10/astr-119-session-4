cabe36f8beffc422f76e7110fb6129a24263023d
Eric Gonzalez Corona <43654373+ericgc10@users.noreply.github.com>
2018-10-09 13:14:58 -0700

    Added 4 files from session 4

---
+++ numpy.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+x = 1.0 #define float
+y = 2.0 #define another float
+
+#trig
+print(np.sin(x))	#sinx
+print(np.cos(x))	#cosx
+print(np.tan(x))	#tanx
+print(np.arccos(x))	#arccosx
+print(np.arcsin(x)) #arcsinx
+print(np.arctan(x)) #arctanx
+print(np.arctan2(x,y)) #arctan(x/y)
+print(np.rad2deg(x)) #convert rad to deg
+
+#hyperbolic functions
+print(np.sinh(x)) #sinh(x)
+print(np.cosh(x)) #cosh(x)
+print(np.tanh(x)) #tanh(x)
+print(np.arcsinh(x)) #arcsinh(x)
+print(np.arccosh(x)) #arccosh(x)
+print(np.arctanh(x)) #arctanh(x)
---
+++ useful_modules.py
@@ -0,0 +1,9 @@
+import numpy as np		#importing numpy library	
+import matplotlib.pyplot as plt		#Matplotlib's pyplot
+
+import sys		#gives access to a C-like sys library	
+import os		#gives access to operating system
+
+
+print(sys.argv)	#print any command line arguments
+print(os.getcwd())	#print the current working directory
